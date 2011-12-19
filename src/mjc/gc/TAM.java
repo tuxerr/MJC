62b9102ba17e8c50c75707475fa036dad90453cf
Guillaume Casanova <Gyombagh@gmail.com>
2011-12-19 17:24:37 +0100

    premieres modifications et test de commit

--- src/mjc/gc/TAM.java
+++ src/mjc/gc/TAM.java
@@ -7,3 +7,14 @@ public class TAM extends AbstractMachine {
 	}
 
 }
+
+//genCst(String v)
+//genOp(String codeF, String codeOp) pour les opérateurs unaires
+//genOp(String codeF, String codeOp, String codeFX) pour les opérateurs binaires
+//genOpPlus()
+//genOpMoins()
+//genOpNon()
+//genOpMul()
+//genOpDiv()
+//genOpMod()
+//genOpEt()
