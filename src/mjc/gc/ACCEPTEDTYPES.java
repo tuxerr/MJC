c0338c43ced4b341517984b2b85cc96ac8ce71cb
Rémi Palandri <palandrir@gmail.com>
2011-12-29 16:51:14 +0100

    pushed the contains function for ACCEPTEDTYPES

--- src/mjc/gc/ACCEPTEDTYPES.java
+++ src/mjc/gc/ACCEPTEDTYPES.java
@@ -3,5 +3,12 @@ package mjc.gc;
 import java.util.ArrayList;
 
 public class ACCEPTEDTYPES extends ArrayList<DTYPE> {
-    public boolean contains(DTYPE type) { };
+    public boolean contains(DTYPE type) { 
+        for(DTYPE d : this) {
+            if (d.equals(type)) {
+                return true;
+            } 
+        }
+        return false;
+    };
 }
